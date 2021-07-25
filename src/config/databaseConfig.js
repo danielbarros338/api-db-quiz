4718cb7f15dba716a907fc595d5c08742c69ad6c
danielbarros338 <danielbarros338@gmail.com>
2021-07-25 19:10:23 -0300

    ref databaseConfig

--- src/config/databaseConfig.js
+++ src/config/databaseConfig.js
@@ -1,7 +1,7 @@
 const { Sequelize }  = require('sequelize');
                                                                        
-module.exports = sequelize = new Sequelize('quiz','admin','senhadb1',{
-    host:'quizdb.cikcxrvgzer3.sa-east-1.rds.amazonaws.com',
+module.exports = sequelize = new Sequelize('heroku_4df41feca0a57fe','b9efb83771cd40','93b2f75d',{
+    host:'us-cdbr-east-04.cleardb.com',
     port:'3306',
     dialect: 'mysql'
 });
